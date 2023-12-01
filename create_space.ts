f0501217530caec15b720df7b2927097be557957
Adam <adam@unchained.work>
2023-12-01 13:49:53 -0800

    added code to create space

---
+++ create_space.ts
@@ -0,0 +1,30 @@
+import { API_KEY, Build5, https } from '@build-5/client';
+import { Dataset, Space } from '@build-5/interfaces';
+import { address } from './utils/secret';
+import { walletSign } from './utils/utils';
+
+async function main() {
+  const origin = Build5.TEST;
+  let response: Space;
+  const userSign = await walletSign(address.bech32, address);
+  try {
+    response = await https(origin)
+      .dataset(Dataset.SPACE)
+      .create({
+        address: address.bech32,
+        signature: userSign.signature,
+        // Use SOONAVERSE TEST - wen.soonaverse.com
+        projectApiKey: API_KEY[origin],
+        body: {
+          name: 'TanKRURK',
+        },
+      });
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+
+  console.log(response);
+}
+
+main();
